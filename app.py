a3eec5fa01441894df91b1a0059f5eebb8aa5a0b
xychew0510 <99796779+xychew0510@users.noreply.github.com>
2022-03-09 16:03:01 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+from flask import render_template, request
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from werkzeug.utils import secure_filename
+import speech_recognition as sr
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        print("File Received")
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save("static/" + filename)
+        a = sr.AudioFile("static/" + filename)
+        with a as source:
+            a = sr.Recognizer().record(source)
+        s = sr.Recognizer().recognize_google(a)
+        return(render_template("index.html", result=s))
+    else:
+        return(render_template("index.html", result="2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
